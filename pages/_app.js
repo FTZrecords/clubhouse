fe4f7e2961c4c11a4bcaa49527ced34fc3ac4251
FTZrecords <ftzrecords@gmail.com>
2021-08-23 15:23:38 +0900

    add All

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
+import Router from "next/router";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import "styles/reset.scss";
+import "styles/loadbar.scss";
+import "styles/global.scss";
+
+Router.events.on("routeChangeStart", loadStart);
+Router.events.on("routeChangeComplete", loadDone);
+Router.events.on("routeChangeError", loadDone);
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+function loadStart() {
+  document.getElementById("loadbar").className = "start";
+}
+function loadDone() {
+  document.getElementById("loadbar").className = "done";
+}
